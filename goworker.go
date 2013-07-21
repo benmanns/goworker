01cd06ba0b6bf41c08181a0cc635237a041e909c
Benjamin Manns <benmanns@gmail.com>
2013-07-21 15:17:34 -0400

    Add seelog logger.

--- goworker.go
+++ goworker.go
@@ -1,5 +1,18 @@
 package goworker
 
+import (
+	"github.com/cihub/seelog"
+	"os"
+)
+
+var logger seelog.LoggerInterface
+
 func Work() error {
+	var err error
+	logger, err = seelog.LoggerFromWriterWithMinLevel(os.Stdout, seelog.InfoLvl)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
