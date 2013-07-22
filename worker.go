82103c67523010374f6816c71db9f06704154884
Benjamin Manns <benmanns@gmail.com>
2013-07-22 11:59:34 -0400

    Add status methods to worker.

--- worker.go
+++ worker.go
@@ -2,6 +2,8 @@ package goworker
 
 import (
 	"encoding/json"
+	"fmt"
+	"time"
 )
 
 type worker struct {
@@ -20,4 +22,56 @@ func newWorker(id string, queues []string) (*worker, error) {
 
 func (w *worker) MarshalJSON() ([]byte, error) {
 	return json.Marshal(w.String())
+}
+
+func (w *worker) start(conn *redisConn, job *job) error {
+	work := &work{
+		Queue:   job.Queue,
+		RunAt:   time.Now(),
+		Payload: job.Payload,
+	}
+
+	buffer, err := json.Marshal(work)
+	if err != nil {
+		return err
+	}
+
+	conn.Send("SET", fmt.Sprintf("resque:worker:%s", w), buffer)
+	logger.Debugf("Processing %s since %s [%v]", work.Queue, work.RunAt, work.Payload.Class)
+
+	return w.process.start(conn)
+}
+
+func (w *worker) fail(conn *redisConn, job *job, err error) error {
+	failure := &failure{
+		FailedAt:  time.Now(),
+		Payload:   job.Payload,
+		Exception: "Error",
+		Error:     err.Error(),
+		Worker:    w,
+		Queue:     job.Queue,
+	}
+	buffer, err := json.Marshal(failure)
+	if err != nil {
+		return err
+	}
+	conn.Send("RPUSH", "resque:failed", buffer)
+
+	return w.process.fail(conn)
+}
+
+func (w *worker) succeed(conn *redisConn, job *job) error {
+	conn.Send("INCR", "resque:stat:processed")
+	conn.Send("INCR", fmt.Sprintf("resque:stat:processed:%s", w))
+
+	return nil
+}
+
+func (w *worker) finish(conn *redisConn, job *job, err error) error {
+	if err != nil {
+		w.fail(conn, job, err)
+	} else {
+		w.succeed(conn, job)
+	}
+	return w.process.finish(conn)
 }
