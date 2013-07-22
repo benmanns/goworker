51807d4e7acf48b737422896ac4f474fd2cf20b4
Benjamin Manns <benmanns@gmail.com>
2013-07-22 10:43:22 -0400

    Create newPoller initializer.

--- poller.go
+++ poller.go
@@ -3,3 +3,13 @@ package goworker
 type poller struct {
 	process
 }
+
+func newPoller(queues []string) (*poller, error) {
+	process, err := newProcess("poller", queues)
+	if err != nil {
+		return nil, err
+	}
+	return &poller{
+		process: *process,
+	}, nil
+}
