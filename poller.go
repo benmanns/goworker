26da1dff1b57b2acafa48d0a0a30f17917fe3544
Benjamin Manns <benmanns@gmail.com>
2013-07-22 11:21:45 -0400

    Create poller.getJob method.

--- poller.go
+++ poller.go
@@ -1,5 +1,10 @@
 package goworker
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type poller struct {
 	process
 }
@@ -12,4 +17,27 @@ func newPoller(queues []string) (*poller, error) {
 	return &poller{
 		process: *process,
 	}, nil
+}
+
+func (p *poller) getJob(conn *redisConn) (*job, error) {
+	for _, queue := range p.Queues {
+		logger.Debugf("Checking %s", queue)
+
+		reply, err := conn.Do("LPOP", fmt.Sprintf("resque:queue:%s", queue))
+		if err != nil {
+			return nil, err
+		}
+		if reply != nil {
+			logger.Debugf("Found job on %s", queue)
+
+			job := &job{Queue: queue}
+
+			if err := json.Unmarshal(reply.([]byte), &job.Payload); err != nil {
+				return nil, err
+			}
+			return job, nil
+		}
+	}
+
+	return nil, nil
 }
