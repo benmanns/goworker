dd607224113c85d7cd7144dd12e98bae0ccc23d5
Benjamin Manns <benmanns@gmail.com>
2013-07-22 10:40:40 -0400

    Create newProcess initializer.

--- process.go
+++ process.go
@@ -1,8 +1,26 @@
 package goworker
 
+import (
+	"os"
+)
+
 type process struct {
 	Hostname string
 	Pid      int
 	Id       string
 	Queues   []string
+}
+
+func newProcess(id string, queues []string) (*process, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return nil, err
+	}
+
+	return &process{
+		Hostname: hostname,
+		Pid:      os.Getpid(),
+		Id:       id,
+		Queues:   queues,
+	}, nil
 }
