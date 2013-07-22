20ea79612b898071a19393e258ad7769776b5f08
Benjamin Manns <benmanns@gmail.com>
2013-07-22 11:39:49 -0400

    Create newWorker constructor.

--- worker.go
+++ worker.go
@@ -3,3 +3,13 @@ package goworker
 type worker struct {
 	process
 }
+
+func newWorker(id string, queues []string) (*worker, error) {
+	process, err := newProcess(id, queues)
+	if err != nil {
+		return nil, err
+	}
+	return &worker{
+		process: *process,
+	}, nil
+}
