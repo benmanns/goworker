9f341a7e8820288458d0b8cce9a3d02dd66ea485
Olivier Amblet <olivier@amblet.net>
2013-12-19 16:54:19 +0100

    Added enqueue file

---
+++ enqueue.go
@@ -0,0 +1,33 @@
+package goworker
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+/*
+Enqueue function let you enqueue a new job in Resque given
+the queue, the class name and the parameters.
+*/
+func Enqueue(queue string, class string, args []interface{}) error {
+	conn, err := redisConnFromUri(uri)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	data := &payload{
+		Class: class,
+		Args:  args,
+	}
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	err = conn.Send("RPUSH", fmt.Sprintf("resque:queue:%s", queue), b)
+	conn.Flush()
+	if err != nil {
+		return err
+	}
+	return nil
+}
