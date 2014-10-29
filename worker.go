7ba08730d366744059f2b9feae673689a80328d5
Nick Walker <nick.walker@vendhq.com>
2014-10-29 14:40:41 +1300

    Add additional logging on worker errors

--- worker.go
+++ worker.go
@@ -151,5 +151,9 @@ func (w *worker) run(job *Job, workerFunc workerFunc) {
 		w.start(conn, job)
 		PutConn(conn)
 	}
+
 	err = workerFunc(job.Queue, job.Payload.Args...)
+	if err != nil {
+		logger.Criticalf("Got error from worker %v", err)
+	}
 }
